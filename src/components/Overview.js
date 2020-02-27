90c6eb468fc7699a79a48dba3877c64e3559df41
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-02-27 11:20:06 +0000

    tests running, after brew install watchman, also snubbed css and other files to allow tests to continue running

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -6,7 +6,7 @@ export default function Overview(props) {
         <div>
             <p>showing overview component</p>
             <ul>
-                {props.info.map((information, index) => <li key={index}>Name and Monthly Price: {information.name}, {information.expectedMonthlySpend}</li>)}
+                {props.info.map((information, index) => <li key={index}>Name: {information.name}</li>)}
             </ul>
         </div>
     )
--- tests/App.test.js
+++ tests/App.test.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import App from '../src/containers/App/App'
-
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../src/containers/App/App';
+import Navbar from '../src/components/Navbar';
+import Overview from '../src/components/Overview';
+import waitUntil from "async-wait-until";
 
 describe("shallow app", () => {
     let wrapper;
@@ -11,5 +13,14 @@ describe("shallow app", () => {
     it("should render correctly", () => {
         expect(wrapper).toMatchSnapshot();
     })
+    it("Should render Navbar component", () => {
+        expect(wrapper.containsMatchingElement(<Navbar />)).toEqual(true);
+    })
+    it('should render an Overview component if the fetch was successful'),
+    async () => {
+        wrapper.setState({fetched: true})
+        await waitUntil(() => wrapper.instance().state.fetched)
+        expect(wrapper.containsMatchingElement(<Overview />)).toEqual(true);
+    }
 });
 
