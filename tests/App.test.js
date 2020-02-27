36fead1be08a1132265551b9875ef6f737102c04
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-02-27 12:54:25 +0000

    working API fetch mock test

--- tests/App.test.js
+++ tests/App.test.js
@@ -7,8 +7,10 @@ import waitUntil from "async-wait-until";
 
 describe("shallow app", () => {
     let wrapper;
+    let getAllDataMock = jest.fn()
+
     beforeEach(() => {
-        wrapper = shallow(<App />)
+        wrapper = shallow(<App getAllData={getAllDataMock}/>)
     })
     it("should render correctly", () => {
         expect(wrapper).toMatchSnapshot();
@@ -24,3 +26,32 @@ describe("shallow app", () => {
     // }
 });
 
+
+// <========================== API TESTING =============================>
+
+function APIRequest () {
+    let url = "https://demo.staging.energyhelpline-aws.com/api/results/a8823b4b-1abe-41de-a5b3-ab6700c08d98";
+    return fetch(url).then(res => res.json());
+  };
+
+describe("testing API", () => {
+    beforeEach(() => {
+      fetch.resetMocks();
+    });
+    // API Request test will fail needs to find way to export function from class
+    it("calls API and returns data to me", () => {
+      //Mock fetch response data
+      fetch.mockResponseOnce(JSON.stringify({ data: "placeholder12345" }));
+  
+    //   //assert on the response
+      APIRequest().then(res => {
+        expect(res.data).toEqual("placeholder12345");
+      });
+
+      //assert on the time called and arguments given to fetch
+      expect(fetch.mock.calls.length).toEqual(1); // fetch.mock.calls = nested array [[]] which contains the URL.
+      expect(fetch.mock.calls[0][0]).toEqual(
+        'https://demo.staging.energyhelpline-aws.com/api/results/a8823b4b-1abe-41de-a5b3-ab6700c08d98'
+      );
+    });
+  });
