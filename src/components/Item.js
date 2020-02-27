0e3679aca70d5bbb077b1a99ae309ae4fd26d194
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-02-27 11:53:39 +0000

    7 passing tests

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Item(props) {
     return (
         <div>
-            <p>showing Item component</p>
+            <p>showing iitem component</p>
             <ul>
                 {props.allInfo.map((allInfo, index) => <li key={index}>Name = {allInfo.name}</li>)}
             </ul>
--- tests/Item.test.js
+++ tests/Item.test.js
@@ -4,8 +4,52 @@ import Item from '../src/components/Item';
 
 describe("shallow Item", () => {
     let wrapper;
+    let allInfo = [{
+        id: "tariffSelectionE716598",
+        supplyDetailsId: "1795",
+        canApply: true,
+        name: "Covalent Capped With Fee",
+        logoUri: "https://rest.staging.energyhelpline.com/Uploads/SupplierLogos/Woodland_TRANS.gif?t=QtT9F_RfNGiloVcwLpCkGELowyI",
+        expectedAnnualSavings: 112,
+        expectedAnnualSpend: "469.67",
+        expectedMonthlySpend: "39.14",
+        messages: [
+        "Early exit fee: £30.00"
+        ],
+        tags: [ ],
+        tariffType: "unknown",
+        paymentMethod: "Monthly Direct Debit",
+        supplierName: "Covalent Energy",
+        supplierId: "136",
+        isBigSix: false,
+        hasPaperBilling: true,
+        hasExitFees: true
+        },
+        {
+        id: "tariffSelectionE716668",
+        supplyDetailsId: "1796",
+        canApply: true,
+        name: "Covalent Fixed Six Months",
+        logoUri: "https://rest.staging.energyhelpline.com/Uploads/SupplierLogos/Woodland_TRANS.gif?t=QtT9F_RfNGiloVcwLpCkGELowyI",
+        expectedAnnualSavings: 112,
+        expectedAnnualSpend: "469.67",
+        expectedMonthlySpend: "39.14",
+        messages: [
+        "No early exit fees",
+        "Fixed until 26 February 2017"
+        ],
+        tags: [ ],
+        tariffType: "fixed",
+        paymentMethod: "Monthly Direct Debit",
+        supplierName: "Covalent Energy",
+        supplierId: "136",
+        isBigSix: false,
+        hasPaperBilling: true,
+        hasExitFees: false
+        }]
+
     beforeEach(() => {
-        wrapper = shallow(<Item />)
+        wrapper = shallow(<Item allInfo={allInfo}/>)
     })
     it("should render correctly", () => {
         expect(wrapper).toMatchSnapshot();
